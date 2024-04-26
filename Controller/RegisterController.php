6e8b36aef205c5f06c49615997250259d29e6a47
sridharan01234 <sridharan01234@gmail.com>
2024-04-26 18:25:39 +0530

    Registration controller for handling register page added

---
+++ Controller/RegisterController.php
@@ -0,0 +1,58 @@
+<?php
+require "../Model/UserModel.php";
+require '../vendor/autoload.php';
+class RegisterController
+{
+    private $userModel;
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
+
+    public function index()
+    {
+        require_once "../View/register.php";
+    }
+
+    public function register()
+    {
+        $data = [
+            "name" => $_POST["name"],
+            "email" => $_POST["email"],
+            "password" => password_hash($_POST["password"], PASSWORD_DEFAULT),
+            "first_name" => $_POST["first_name"],
+            "last_name" => $_POST["last_name"],
+            "name"=> $_POST["first_name"].$_POST["last_name"]
+        ];
+        if (!$this->userModel->get("users", ["email" => $data["email"]], "*")) {
+            if($this->userModel->insert("users", $data, 1))
+            {
+                echo"Email Successfully Registered";
+            }
+        } else {
+            echo "Email Already Registered";
+        }
+    }
+}
+
+$init = new RegisterController();
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    switch ($_GET['type']) {
+        case 'index':
+            $init->index();
+            break;
+        default:
+            header('location: ../');
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    switch ($_POST['type']) {
+        case 'register':
+            $init->register();
+            break;
+        default:
+            header('location: ../');
+    }
+}
