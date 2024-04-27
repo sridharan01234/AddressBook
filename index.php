201b890cfb2e1874d1cd0214e681e95bcdd735e8
sridharan01234 <sridharan01234@gmail.com>
2024-04-27 14:30:23 +0530

    index Added

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="Controller/RegisterController.php" method="get">
-        <input type="hidden" name="type" value="index">
-        <button>Register Page</button>
-    </form>
-</body>
-</html>
+<form action="Controller/LoginController.php" method="get"><button>Login</button></form>
