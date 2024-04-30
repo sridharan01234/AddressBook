c5f361d7b8d35f6a17df149ec53050739924e3ee
sridharan01234 <sridharan01234@gmail.com>
2024-04-30 11:00:49 +0530

    form shaking added when empty fields

--- Assets/js/listusers.js
+++ Assets/js/listusers.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById('defaultModalButton').click();
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.getElementById("defaultModalButton").click();
 });
--- Assets/js/register.js
+++ Assets/js/register.js
@@ -1,12 +1,56 @@
 function validateRegisterForm() {
+  document.getElementById("error").style.display = "none";
   if (
-    document.getElementById("inputFirstName").value == "" ||
-    document.getElementById("inputLastName").value == "" ||
-    document.getElementById("inputEmail").value == "" ||
-    document.getElementById("inputPassword").value == "" ||
-    document.getElementById("ConfirmPassword").value == ""
+    document.getElementById("floating_first_name").value == "" ||
+    document.getElementById("floating_last_name").value == "" ||
+    document.getElementById("floating_email").value == "" ||
+    document.getElementById("floating_password").value == "" ||
+    document.getElementById("floating_repeat_password").value == ""
   ) {
-    alert("All the Fields must be Filled");
+    document.getElementById("error").style.display = "block";
+    document.getElementById("error").innerHTML =
+      "All the fields must be filled";
+    shakeForm();
     return false;
   }
+  if (validatePassword) return false;
+}
+
+function shakeForm() {
+  var margin = 15;
+  var speed = 10;
+  var times = 3;
+  for (var i = 0; i < times; i++) {
+    $("form").animate(
+      { "margin-left": "+=" + (margin = -margin) + "px" },
+      speed
+    );
+    $("form").animate(
+      { "margin-right": "+=" + (margin = -margin) + "px" },
+      speed
+    );
+    $("form").animate(
+      { "margin-right": "+=" + (margin = -margin) + "px" },
+      speed
+    );
+    $("form").animate(
+      { "margin-left": "+=" + (margin = -margin) + "px" },
+      speed
+    );
+  }
+}
+
+function validatePassword() {
+  var lowercaseRegex = /[a-z]/;
+  var uppercaseRegex = /[A-Z]/;
+  var numericRegex = /[0-9]/;
+  var specialCharacterRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+  var password = document.getElementById("floating_password").value;
+  if (
+    lowercaseRegex.test(password) &&
+    uppercaseRegex.test(password) &&
+    numericRegex.test(password) &&
+    specialCharacterRegex.test(password)
+  )
+    return false;
 }
