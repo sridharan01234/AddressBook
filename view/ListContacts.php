171cb207529f4c2f20ee1631e8a2165bc482f427
sridharan01234 <sridharan01234@gmail.com>
2024-05-10 12:28:26 +0530

    function added

--- view/ListContacts.php
+++ view/ListContacts.php
@@ -21,7 +21,7 @@
         </div>
         <div class="grid grid-cols-2 gap-3">
 
-            <form id="delete" onsubmit="deleteAlert()" action="deleteContacts" method="post">
+            <form id="delete" onsubmit="return deleteAlert()" action="deleteContacts" method="post">
                 <input type="hidden" name="type" value="delete">
                 <button type="submit"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
