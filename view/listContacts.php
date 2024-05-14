8fe1c8974793745dca24ff73c21559c1e1e9df56
sridharan01234 <sridharan01234@gmail.com>
2024-05-14 15:25:26 +0530

    div changed

--- view/listContacts.php
+++ view/listContacts.php
@@ -17,15 +17,15 @@
         Manage all your existing Contacts or add a new one
       </p>
     </div>
-    <div class="flex flex-row items-center justify-between space-x-4 sm:space-x-0">
+    <div class="flex flex-row itemsnvm ls-remote-center justify-between space-x-4 sm:space-x-0">
+      <form id="delete" onsubmit="return deleteAlert()" action="/deleteContact" method="post">
+        <button type="submit" class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-red-400 dark:hover:bg-red-500 dark:focus:ring-red-600">Delete Selected</button>
+      </form>
       <a href="/logout">
         <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Logout
         </button>
       </a>
-      <form id="delete" onsubmit="return deleteAlert()" action="/deleteContact" method="post">
-        <button type="submit" class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-red-400 dark:hover:bg-red-500 dark:focus:ring-red-600">Delete Selected</button>
-      </form>
     </div>
 
   </div>
