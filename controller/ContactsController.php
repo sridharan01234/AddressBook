ddeee99ab43a8d29188355e891b879a3b784b59d
sridharan01234 <sridharan01234@gmail.com>
2024-05-15 10:24:49 +0530

    GET class constant added

--- controller/ContactsController.php
+++ controller/ContactsController.php
@@ -17,6 +17,7 @@ class ContactsController extends BaseController
 {
     
     private const POST = "POST";
+    private const GET = "GET";
     private $contactsModel;
 
     /**
@@ -81,6 +82,36 @@ class ContactsController extends BaseController
             }
          else {
             $this->render("addContact",[]);
+        }
+    }
+
+    /**
+     * Edit a contact
+     * 
+     * @return void
+     */
+    public function editContact(): void
+    {
+        if($_SERVER["REQUEST_METHOD"] == self::GET)
+        {
+            $this->render("editContact",[
+                "contact" => $this->contactsModel->getContacts($_SESSION['user_id'])[0]
+            ]);
+        }
+        if ($_SERVER["REQUEST_METHOD"] == self::POST) 
+        {
+            $data = [
+                "name"=> $_POST["name"],
+                "phone"=> $_POST["phone"],
+                "age"=> $_POST["age"],
+                "pincode"=> $_POST["pincode"],
+                "address"=> $_POST["address"],
+                "country_id"=> $_POST["country"],
+                "state_id"=> $_POST["state"],
+                "user_id"=> $_SESSION['user_id'],
+                ];
+            $this->contactsModel->updateContacts($_POST['id'],$data);
+            $this->listContacts();
         }
     }
 
