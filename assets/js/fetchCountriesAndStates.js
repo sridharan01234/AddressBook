894e25dea9a56b29f2cf7ed0d3c322c2d9039b59
sridharan01234 <sridharan01234@gmail.com>
2024-05-17 11:09:24 +0530

    Refactor AJAX  in fetchCountriesAndStates.js

--- assets/js/fetchCountriesAndStates.js
+++ assets/js/fetchCountriesAndStates.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $.ajax({
         url: 'getCounties',
         type: 'GET',
-        success: function(response) {
+        success: function (response) {
             var countries = JSON.parse(response);
             populateCountries(countries);
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             console.error("Error fetching countries: " + error);
         }
     });
 
     function populateCountries(countries) {
         var select = $('#countrySelect');
-        $.each(countries, function(index, country) {
+        $.each(countries, function (index, country) {
             select.append('<option value="' + country.id + '">' + country.name + '</option>');
         });
-        
-        select.change(function() {
+
+        select.change(function () {
             var countryId = $(this).val();
             fetchStates(countryId);
         });
@@ -28,11 +28,11 @@ $(document).ready(function(){
             url: 'getStates',
             type: 'GET',
             data: { country_id: countryId },
-            success: function(response) {
+            success: function (response) {
                 var states = JSON.parse(response);
                 populateStates(states);
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.error("Error fetching states: " + error);
             }
         });
@@ -41,13 +41,13 @@ $(document).ready(function(){
     function populateStates(states) {
         var select = $('#stateSelect');
         select.empty();
-        $.each(states, function(index, state) {
+        $.each(states, function (index, state) {
             console.log(state);
             select.append('<option value="' + state.id + '">' + state.name + '</option>');
         });
     }
 
-    $('#stateSelect').on('focus', function() {
+    $('#stateSelect').on('focus', function () {
         $(this).val('');
     });
 });
