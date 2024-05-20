1efe7b37be4273e2c721e1c80626359dbe5a3b5b
sridharan <sridharan01234@gmail.com>
2024-05-20 15:00:15 +0530

    Fix: Eliminate extraneous whitespace in routing definition

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $router->add("/addContact", ['Controller' => 'ContactsController', 'action' => '
 $router->add("/deleteContact", ['Controller' => 'ContactsController', 'action' => 'deleteContact']);
 $router->add("/countries", ['Controller' => 'ContactsController', 'action' => 'getCounties']);
 $router->add("/states", ['Controller' => 'ContactsController', 'action' => 'getStates']);
-~
+
 $param = $router->searchPath($path);
 if (!$param) {
     require_once './view/pageNotFound.php';
