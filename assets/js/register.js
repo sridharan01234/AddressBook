543060a914a82784de088d5c586e92b2f4c5c388
sridharan <sridharan01234@gmail.com>
2024-05-24 12:08:40 +0530

    feat: Add user existence validation to registration

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,52 +1,62 @@
-$("#register-form").validate(
-  {
-    rules: {
-      first_name: "required",
-      last_name: "required",
-      email: "required",
-      password: {
-        strongPassword: true,
-        required: true,
-        minlength: 8,
-      },
-      repeat_password: {
-        required: true,
-        minlength: 8,
-        equalTo: "#password",
-      },
+$.validator.addMethod("verifyUser", function(value, element) {
+  var email = value;
+  var response = false;
+  $.ajax({
+    url: 'verifyUser',
+    method: 'GET',
+    data: { email: email },
+    async: false,
+    success: function(result) {
+      response = (result === 'exists');
+    },
+    error: function() {
+      response = false;
+    }
+  });
+  return !response;
+});
+
+
+$("#register-form").validate({
+  rules: {
+    first_name: "required",
+    last_name: "required",
+    email: {
+      required: true,
+      email: true,
+      verifyUser: true
+    },
+    password: {
+      strongPassword: true,
+      required: true,
+      minlength: 8,
+    },
+    repeat_password: {
+      required: true,
+      minlength: 8,
+      equalTo: "#password",
     },
   },
-  {
-    messages: {
-      first_name: "This field is required",
-      last_name: "This field is required",
-      email: {
-        required: true,
-        email: true,
-      },
-      password: {
-        required: "this field is required",
-        minlength: "minimum 8 characters required",
-      },
-      repeat_password: {
-        required: "this field is required",
-        minlength: "minimum 8 characters required",
-        equalTo: "password does not match",
-      },
+  messages: {
+    first_name: "This field is required",
+    last_name: "This field is required",
+    email: {
+      required: "This field is required",
+      email: "Please enter a valid email address",
+      verifyUser: "User exists",
     },
-  }
-);
-
-jQuery.validator.addMethod(
-  "strongPassword",
-  function (value, element) {
-    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/.test(
-      value
-    );
+    password: {
+      required: "This field is required",
+      minlength: "Minimum 8 characters required",
+    },
+    repeat_password: {
+      required: "This field is required",
+      minlength: "Minimum 8 characters required",
+      equalTo: "Password does not match",
+    },
   },
-  "Password must contain at least 8 characters including at least one lowercase letter, one uppercase letter, one digit, and one special character."
-);
+});
 
-setTimeout(function () {
+setTimeout(function() {
   $("#error, #message").hide("slow");
 }, 5000);
